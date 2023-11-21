80b22554293dd1ad1992756798322331739f0bed
rqueaArequipa <ruben.quea@tecsup.edu.pe>
2023-11-20 20:57:43 -0500

    LAB12-AMM-PC

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,22 +1,33 @@
 import 'package:flutter/material.dart';
+//import 'package:laboratorio02/app/view/task_list.dart';
 //import 'package:laboratorio02/app/view/home.dart';
-//import 'package:laboratorio02/app/view/splash.dart';
-import 'package:laboratorio02/app/view/welcome.dart';
-
+//import 'package:laboratorio02/app/view/home.dart';
+import 'package:laboratorio02/app/view/splash/Splash_page.dart';
+//import 'package:laboratorio02/app/view/welcome.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    const primary = Color(0xFF40B7AD);
+    const textcolor = Color(0xFF4A4A4A);
+    const backgrouncolor = Color(0xFFF5F5F5);
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: primary),
+        scaffoldBackgroundColor:  backgrouncolor,
+        textTheme: Theme.of(context).textTheme.apply(
+          fontFamily: 'Popponis',
+          bodyColor: textcolor,
+          displayColor: textcolor,
+        ),
         useMaterial3: true,
       ),
       //home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      home: WelcomePage(),
+      home: SplashPage(),
     );
   }
 }
---
+++ lib/app/view/splash/Splash_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:laboratorio02/app/view/components/h1.dart';
+import 'package:laboratorio02/app/view/components/shape.dart';
+import 'package:laboratorio02/app/view/task_list/Task_list_page.dart';
+
+class SplashPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      children: [
+        Row(
+          children: [
+            Shape(),
+          ],
+        ),
+        const SizedBox(height: 79),
+        Image.asset(
+          'assets/images/onboarding-image.png',
+          width: 180,
+          height: 168,
+        ),
+        const SizedBox(height: 99),
+        const H1("Lista de tareas"),
+        const SizedBox(height: 21),
+        const Padding(padding: EdgeInsets.symmetric(horizontal: 32)),
+        GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+              return const TaskListPage();
+            }));
+          },
+          child: const Text(
+            'La mejor forma para que no se te olvide nada es anotarlo. Guardar tus tareas y ve completando poco a poco para aumentar tu productividad',
+            textAlign: TextAlign.center,
+          ),
+        )
+      ],
+    ));
+  }
+}
